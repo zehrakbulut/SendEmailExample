c876bc7e33d68e55a16c930314c57e53306eab3f
Zehra Akbulut <zehrakbulut23@gmail.com>
2025-04-14 15:04:01 +0300

    send email exercise successfull

---
+++ SendEmailExample/Controllers/EmailsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendEmailExample.Services;
+
+namespace SendEmailExample.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EmailsController : ControllerBase
+	{
+		private readonly IEmailService emailService;
+
+		public EmailsController(IEmailService _emailService)
+		{
+			emailService = _emailService;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> SendEmail(string receptor, string subject, string body)
+		{
+			await emailService.SendEmail(receptor, subject, body);
+			return Ok();
+		}
+	}
+}
---
+++ SendEmailExample/Services/EmailService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace SendEmailExample.Services
+{
+	public interface IEmailService
+	{
+		Task SendEmail(string receptor, string subject, string body);
+	}
+
+	public class EmailService : IEmailService
+	{
+		private readonly IConfiguration _configuration;
+
+		public EmailService(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public async Task SendEmail(string receptor, string subject, string body)
+		{
+			var email = _configuration.GetValue<string>("EMAIL_CONFIGURATION:EMAIL");
+			var password = _configuration.GetValue<string>("EMAIL_CONFIGURATION:PASSWORD");
+			var host = _configuration.GetValue<string>("EMAIL_CONFIGURATION:HOST");
+			var port = _configuration.GetValue<int>("EMAIL_CONFIGURATION:PORT");
+
+			var smtpClient = new SmtpClient(host, port);
+			smtpClient.EnableSsl = true;
+			smtpClient.UseDefaultCredentials = false;
+
+			smtpClient.Credentials = new NetworkCredential(email, password);
+
+			var message = new MailMessage(email!, receptor, subject, body);
+			await smtpClient.SendMailAsync(message);
+		}
+	}
+}
